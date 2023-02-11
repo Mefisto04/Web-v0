a0f4de3f67d2a22d0892ca2a65d0b6b0317ccd94
Mefisto04 <kawalemayur30@gmail.com>
2023-02-11 09:52:02 +0000

    Added layout.tsx

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+// import StreamVideoProvider from '@/providers/StreamClientProvider';
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      {children}
+    </main>
+  );
+};
+
+export default RootLayout;
