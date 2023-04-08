e27431c243ebf3d06deae95808ac74dc062c0d71
Mefisto04 <kawalemayur30@gmail.com>
2023-04-08 03:52:27 +0000

    Added page.tsx

---
+++ app/(root)/(home)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import EventList from "@/components/EventList";
+
+export default function EventsPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <EventList />
+    </div>
+  );
+}
